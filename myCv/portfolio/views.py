486b78589a16bbf248f21f9854eebf52d720bf59
Sushant <sushantyadav0611@gmail.com>
2024-01-17 22:17:28 +0530

    added slack notification on portfolio page land

--- myCv/portfolio/views.py
+++ myCv/portfolio/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+import requests
+import json
+import os
 
 # Create your views here.
 
 class index(TemplateView):
     template_name = 'index.html'
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        with open("visitor.json") as file:
+            visitor = json.load(file)
+        visitor["total_visit_count"] += 1
+        requests.get(f"{os.getenv('SLACK_URL')}/slack/send", params={
+            "channel": "home-server",
+            "message": f"You have a visitor. Total visit count : {visitor['total_visit_count']}"
+        })
+        with open("visitor.json","w") as file:
+            json.dump(visitor, file, indent=4)
